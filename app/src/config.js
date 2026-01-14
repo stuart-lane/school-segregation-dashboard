a3d95ada15698ee802c3d7b36c31f4f33000f04a
Stuart Lane <stuartlane0014@gmail.com>
2026-01-14 15:14:44 +0000

    Updates to config file

---
+++ app/src/config.js
@@ -0,0 +1,11 @@
+// Use GitHub raw URLs for data files
+const GITHUB_RAW_BASE = "https://raw.githubusercontent.com/stuart-lane/school-segregation-dashboard/refs/heads/main/segDataPrep/outputs";
+
+export const SPATIAL_DATA_URL = `${GITHUB_RAW_BASE}/spatial_data.geojson`;
+export const CHORO_DATA_URL = `${GITHUB_RAW_BASE}/seg_indices.json`;
+export const FILTERS = `${GITHUB_RAW_BASE}/filters.json`;
+export const PLOT_INFO = `${GITHUB_RAW_BASE}/plot_information.json`;
+
+export const MAP_TILES_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+export const ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+export const BASE_URL = "https://raw.githubusercontent.com/stuart-lane/school-segregation-dashboard/refs/heads/main/segDataPrep";
